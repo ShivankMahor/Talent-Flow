4dc710a8156249c50d7bfc52d8235ce1712a77a5
Shivank <shivank88215@gmail.com>
2025-09-24 23:25:25 +0530

    path change auth provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { AuthProvider } from './app/providers/AuthProvider.jsx'
+import { AuthProvider } from './app/providers/AuthProvider'
 import { makeServer } from "./services/mirage/server";
 import { seedDB } from "./db/seed";
 import { BrowserRouter } from 'react-router-dom'
